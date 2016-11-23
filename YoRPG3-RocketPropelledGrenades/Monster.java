e9b238141b004a89d12a71eac5482e30e7bc0f38
tructdao <tdao@stuy.edu>
2016-11-22 21:12:00 -0500

    Add files via upload

---
+++ YoRPG3-RocketPropelledGrenades/Monster.java
@@ -0,0 +1,26 @@
+public class Monster extends Character{
+   
+    public Monster(){
+       	HP = 150;
+	/*returns a random integer in a range by returning the 
+	  sum of the minimum value and M.r() * (max - min) */
+	strength= (int)(20 + (Math.random() * 45));
+	defense= 20;
+	attackR = 1;
+    }
+
+ public String about(){
+	 return "This is a monstaa";
+ }
+
+	public void specialize(){
+	defense =(int)( defense * 0.8);
+	attackR += .7; }
+
+    public void normalize(){
+	defense = oldDef;
+	attackR = oldAtt; }
+	
+}
+
+    
---
+++ YoRPG3-RocketPropelledGrenades/Tank.java
@@ -0,0 +1,27 @@
+public class Tank extends Character{
+
+    public Tank(){
+	HP = 500;
+	strength = 25;
+	defense = 50;
+	attackR = 0.6;
+    }
+ public Tank(String n){
+	this();
+	name = n;
+    }
+ 
+public String about(){
+	return  "Tank owns an abundance of health points.";
+}
+	
+	public void specialize(){
+	defense =(int)( defense * 0.8);
+	attackR += .7; }
+
+    public void normalize(){
+	defense = oldDef;
+	attackR = oldAtt; }
+	
+	
+}
